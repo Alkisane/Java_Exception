d6e3c2702cd2473040f132f7e3e8f45a2f87da0b
Alkisane <139612716+Alkisane@users.noreply.github.com>
2023-10-28 14:40:41 +0500

    Update and rename SaxException.java to SexException.java

--- SexException.java
+++ SexException.java
@@ -1,10 +1,10 @@
 package javaErrorException.Java_Exception;
 
-public class SaxException extends Exception {
-    public SaxException() {
+public class SexException extends Exception {
+    public SexException() {
     }
 
-    public void saxException(String i) {
+    public void sexException(String i) {
         System.out.println("Exception: SaxNotCorrectData");
         System.out.printf("It`s not correct data: %s", i);
         System.out.println();
